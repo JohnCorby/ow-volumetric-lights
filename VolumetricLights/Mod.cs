c06e16216f4fdf803b7bd621dcb9c2cdd37b75a3
JohnCorby <williamforgames6@gmail.com>
2022-11-24 00:27:17 -0800

    goop

---
+++ VolumetricLights/Mod.cs
@@ -0,0 +1,17 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+
+namespace VolumetricLights;
+
+public class Mod : ModBehaviour
+{
+	public override void Configure(IModConfig config)
+	{
+	}
+
+	private static void ApplyLights()
+	{
+		if (LoadManager.GetCurrentScene() != OWScene.SolarSystem) return;
+
+	}
+}
